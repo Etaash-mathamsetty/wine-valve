91f67136cf83932b68e0787fbc732bbed1f2e350
Louis Lenders <xerox.xerox2000x@gmail.com>
2025-03-18 18:54:18 +0100

    win32u: Add stub for NtUserSetAdditionalForegroundBoostProcesses.
    Microsoft Edge worked if windows version is set to win8.1 (or win7) but
    now crashes in current wine due to this unimplemented function.

--- dlls/win32u/main.c
+++ dlls/win32u/main.c
@@ -2030,6 +2030,11 @@ HWND SYSCALL_API NtUserSetActiveWindow( HWND hwnd )
     SYSCALL_FUNC( NtUserSetActiveWindow );
 }
 
+BOOL SYSCALL_API NtUserSetAdditionalForegroundBoostProcesses( HWND hwnd, DWORD count, HANDLE *handles )
+{
+    SYSCALL_FUNC( NtUserSetAdditionalForegroundBoostProcesses );
+}
+
 HWND SYSCALL_API NtUserSetCapture( HWND hwnd )
 {
     SYSCALL_FUNC( NtUserSetCapture );
