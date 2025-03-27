17ac0e2cf9fe8fc679f79d811631954c1ab69414
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2025-03-27 10:01:54 +0200

    winewayland: Use system cursor shapes when possible.
    Use the cursor-shapes-v1 protocol to tell the compositor which system
    cursor shape to use. If a shape match is not found (or cursor-shapes-v1
    is not available) we fall back to setting the cursor buffer from the
    Windows cursor data as before.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=57648

--- dlls/winewayland.drv/waylanddrv.h
+++ dlls/winewayland.drv/waylanddrv.h
@@ -29,6 +29,7 @@
 #include <wayland-client.h>
 #include <xkbcommon/xkbcommon.h>
 #include <xkbcommon/xkbregistry.h>
+#include "cursor-shape-v1-client-protocol.h"
 #include "pointer-constraints-unstable-v1-client-protocol.h"
 #include "relative-pointer-unstable-v1-client-protocol.h"
 #include "text-input-unstable-v3-client-protocol.h"
@@ -124,6 +125,7 @@ struct wayland_pointer
     struct zwp_confined_pointer_v1 *zwp_confined_pointer_v1;
     struct zwp_locked_pointer_v1 *zwp_locked_pointer_v1;
     struct zwp_relative_pointer_v1 *zwp_relative_pointer_v1;
+    struct wp_cursor_shape_device_v1 *wp_cursor_shape_device_v1;
     HWND focused_hwnd;
     HWND constraint_hwnd;
     BOOL pending_warp;
@@ -206,6 +208,7 @@ struct wayland
     struct wp_color_manager_v1 *wp_color_manager_v1;
     struct xdg_system_bell_v1 *xdg_system_bell_v1;
     struct xdg_activation_v1 *xdg_activation_v1;
+    struct wp_cursor_shape_manager_v1 *wp_cursor_shape_manager_v1;
     struct wayland_seat seat;
     struct wayland_keyboard keyboard;
     struct wayland_pointer pointer;
