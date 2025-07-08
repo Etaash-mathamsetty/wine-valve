e9ead6b9791051115501f2873f9e3a336074ba92
Elizabeth Figura <zfigura@codeweavers.com>
2025-07-07 21:44:10 -0400

    ntsync implementation

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -6167,6 +6167,54 @@ struct get_linux_sync_device_reply
 };
 
 
+struct get_linux_sync_obj_request
+{
+    struct request_header __header;
+    obj_handle_t handle;
+};
+struct get_linux_sync_obj_reply
+{
+    struct reply_header __header;
+    int type;
+    unsigned int access;
+};
+
+
+struct select_inproc_queue_request
+{
+    struct request_header __header;
+    char __pad_12[4];
+};
+struct select_inproc_queue_reply
+{
+    struct reply_header __header;
+};
+
+
+struct unselect_inproc_queue_request
+{
+    struct request_header __header;
+    int          signaled;
+};
+struct unselect_inproc_queue_reply
+{
+    struct reply_header __header;
+};
+
+
+struct get_inproc_alert_event_request
+{
+    struct request_header __header;
+    char __pad_12[4];
+};
+struct get_inproc_alert_event_reply
+{
+    struct reply_header __header;
+    obj_handle_t handle;
+    char __pad_12[4];
+};
+
+
 enum request
 {
     REQ_new_process,
@@ -6476,6 +6524,10 @@ enum request
     REQ_get_fsync_apc_idx,
     REQ_fsync_free_shm_idx,
     REQ_get_linux_sync_device,
+    REQ_get_linux_sync_obj,
+    REQ_select_inproc_queue,
+    REQ_unselect_inproc_queue,
+    REQ_get_inproc_alert_event,
     REQ_NB_REQUESTS
 };
 
@@ -6790,6 +6842,10 @@ union generic_request
     struct get_fsync_apc_idx_request get_fsync_apc_idx_request;
     struct fsync_free_shm_idx_request fsync_free_shm_idx_request;
     struct get_linux_sync_device_request get_linux_sync_device_request;
+    struct get_linux_sync_obj_request get_linux_sync_obj_request;
+    struct select_inproc_queue_request select_inproc_queue_request;
+    struct unselect_inproc_queue_request unselect_inproc_queue_request;
+    struct get_inproc_alert_event_request get_inproc_alert_event_request;
 };
 union generic_reply
 {
@@ -7102,8 +7158,12 @@ union generic_reply
     struct get_fsync_apc_idx_reply get_fsync_apc_idx_reply;
     struct fsync_free_shm_idx_reply fsync_free_shm_idx_reply;
     struct get_linux_sync_device_reply get_linux_sync_device_reply;
+    struct get_linux_sync_obj_reply get_linux_sync_obj_reply;
+    struct select_inproc_queue_reply select_inproc_queue_reply;
+    struct unselect_inproc_queue_reply unselect_inproc_queue_reply;
+    struct get_inproc_alert_event_reply get_inproc_alert_event_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 858
+#define SERVER_PROTOCOL_VERSION 861
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
