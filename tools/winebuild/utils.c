38cb6ceea27a20d4fed87591b68fdaa300a877f2
Alexandre Julliard <julliard@winehq.org>
2025-07-18 11:43:20 +0200

    winebuild: Unify the get_stub_name() and get_link_name() helpers.

--- tools/winebuild/utils.c
+++ tools/winebuild/utils.c
@@ -680,7 +680,7 @@ char *make_c_identifier( const char *str )
  *
  * Generate an internal name for a stub entry point.
  */
-const char *get_stub_name( const ORDDEF *odp, const DLLSPEC *spec )
+static const char *get_stub_name( const ORDDEF *odp )
 {
     static char *buffer;
 
@@ -694,7 +694,7 @@ const char *get_stub_name( const ORDDEF *odp, const DLLSPEC *spec )
         if (!*p) return buffer;
         free( buffer );
     }
-    buffer = strmake( "__wine_stub_%s_%d", make_c_identifier(spec->file_name), odp->ordinal );
+    buffer = strmake( "__wine_stub_%d", odp->ordinal );
     return buffer;
 }
 
