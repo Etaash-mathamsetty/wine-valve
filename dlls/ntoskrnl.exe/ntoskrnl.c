c0a541254ec226970b8e4288d97957f517078e79
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2025-07-22 16:14:35 -0400

    ntoskrnl: Implement PsGetCurrentThreadProcess.

--- dlls/ntoskrnl.exe/ntoskrnl.c
+++ dlls/ntoskrnl.exe/ntoskrnl.c
@@ -3275,6 +3275,14 @@ TEB *WINAPI PsGetCurrentThreadTeb(void)
 }
 
 /***********************************************************************
+ *           PsGetCurrentThreadProcess   (NTOSKRNL.EXE.@)
+ */
+PEPROCESS WINAPI PsGetCurrentThreadProcess(void)
+{
+    return KeGetCurrentThread()->process;
+}
+
+/***********************************************************************
  *           PsIsSystemThread   (NTOSKRNL.EXE.@)
  */
 BOOLEAN WINAPI PsIsSystemThread(PETHREAD thread)
