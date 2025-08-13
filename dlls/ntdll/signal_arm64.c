cbe2ead068d2d08360cd2e9c5e1fa8f735f8b06e
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2025-08-12 21:30:43 -0400

    Revert "ntdll: Improve faults logging."
    This reverts commit c3ec8bace870de71a95ce04e55c0044f48abdbba.

--- dlls/ntdll/signal_arm64.c
+++ dlls/ntdll/signal_arm64.c
@@ -110,11 +110,9 @@ __ASM_GLOBAL_FUNC( RtlCaptureContext,
 /**********************************************************************
  *           virtual_unwind
  */
-static NTSTATUS virtual_unwind( ULONG type, DISPATCHER_CONTEXT *dispatch, CONTEXT *context,
-                                BOOL dump_backtrace )
+static NTSTATUS virtual_unwind( ULONG type, DISPATCHER_CONTEXT *dispatch, CONTEXT *context )
 {
     DISPATCHER_CONTEXT_NONVOLREG_ARM64 *nonvol_regs;
-    LDR_DATA_TABLE_ENTRY *module = NULL;
     DWORD64 pc = context->Pc;
     int i;
 
@@ -128,15 +126,6 @@ static NTSTATUS virtual_unwind( ULONG type, DISPATCHER_CONTEXT *dispatch, CONTEX
     for (i = 0; i < 8; i++) nonvol_regs->FpNvRegs[i] = context->V[i + 8].D[0];
 
     dispatch->FunctionEntry = RtlLookupFunctionEntry( pc, &dispatch->ImageBase, dispatch->HistoryTable );
-
-    if (dump_backtrace)
-    {
-        if (!LdrFindEntryForAddress( (void *)pc, &module ))
-            WINE_BACKTRACE_LOG( "%p: %s + %p.\n", (void *)pc, debugstr_w(module->BaseDllName.Buffer),
-                                (void *)((char *)pc - (char *)module->DllBase) );
-        else
-            WINE_BACKTRACE_LOG( "%p: unknown module.\n", (void *)pc );
-    }
 
     if (RtlVirtualUnwind2( type, dispatch->ImageBase, pc, dispatch->FunctionEntry, context,
                            NULL, &dispatch->HandlerData, &dispatch->EstablisherFrame,
@@ -232,7 +221,7 @@ NTSTATUS call_seh_handlers( EXCEPTION_RECORD *rec, CONTEXT *orig_context )
 
     for (;;)
     {
-        status = virtual_unwind( UNW_FLAG_EHANDLER, &dispatch, &context, need_backtrace( rec->ExceptionCode ) );
+        status = virtual_unwind( UNW_FLAG_EHANDLER, &dispatch, &context );
         if (status != STATUS_SUCCESS) return status;
 
     unwind_done:
@@ -506,7 +495,7 @@ void WINAPI RtlUnwindEx( PVOID end_frame, PVOID target_ip, EXCEPTION_RECORD *rec
 
     for (;;)
     {
-        status = virtual_unwind( UNW_FLAG_UHANDLER, &dispatch, &new_context, FALSE );
+        status = virtual_unwind( UNW_FLAG_UHANDLER, &dispatch, &new_context );
         if (status != STATUS_SUCCESS) raise_status( status, rec );
 
     unwind_done:
