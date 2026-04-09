wine-mirror/wine

ntdll: Fix an off-by-one error in the pseudo-handle check for inproc syncs.
Also add an is_pseudo_handle() helper.

dlls/ntdll/unix/sync.c
@@ -556,6 +556,11 @@ static inline unsigned int inproc_sync_handle_to_index( HANDLE handle, unsigned 
     return idx % INPROC_SYNC_CACHE_BLOCK_SIZE;
 }
 
+static BOOL is_pseudo_handle( HANDLE handle )
+{
+    return ((ULONG)(ULONG_PTR)handle >= 0xfffffffa);
+}
+
 static struct inproc_sync *cache_inproc_sync( HANDLE handle, struct inproc_sync *sync )
 {
     unsigned int entry, idx = inproc_sync_handle_to_index( handle, &entry );
@@ -563,7 +568,7 @@ static struct inproc_sync *cache_inproc_sync( HANDLE handle, struct inproc_sync 
     int refcount;
 
     /* don't cache pseudo-handles; waiting on them is pointless anyway */
-    if ((ULONG)(ULONG_PTR)handle > 0xfffffffa) return sync;
+    if (is_pseudo_handle( handle )) return sync;
 
     if (entry >= INPROC_SYNC_CACHE_ENTRIES)
     {

Etaash-mathamsetty/wine-valve

dlls/ntdll/unix/sync.c
@@ -416,6 +416,12 @@ static inline unsigned int inproc_sync_handle_to_index( HANDLE handle, unsigned 
     return idx % INPROC_SYNC_CACHE_BLOCK_SIZE;
 }
 
+
+static BOOL is_pseudo_handle( HANDLE handle )
+{
+    return ((ULONG)(ULONG_PTR)handle >= 0xfffffffa);
+}
+
 static struct inproc_sync_cache_entry *cache_inproc_sync_obj( HANDLE handle, int fd,
                                                               enum inproc_sync_type type, unsigned int access )
 {
@@ -424,8 +430,7 @@ static struct inproc_sync_cache_entry *cache_inproc_sync_obj( HANDLE handle, int
     int refcount;
 
     /* don't cache pseudo-handles; waiting on them is pointless anyway */
-    if ((ULONG)(ULONG_PTR)handle > 0xfffffffa)
-        return FALSE;
+    if (is_pseudo_handle( handle )) return FALSE;
 
     if (entry >= INPROC_SYNC_CACHE_ENTRIES)
     {