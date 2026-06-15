0ec627c7055ed989a0ea04742b555fcb9011e9c0
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2026-06-15 03:09:16 -0400

    winewayland: Store the pending warp position.

--- dlls/winewayland.drv/wayland_pointer.c
+++ dlls/winewayland.drv/wayland_pointer.c
@@ -1018,6 +1018,8 @@ BOOL WAYLAND_SetCursorPos(INT x, INT y)
         return FALSE;
     }
     pointer->pending_warp = TRUE;
+    pointer->warp.x = x;
+    pointer->warp.y = y;
     pthread_mutex_unlock(&pointer->mutex);
 
     TRACE("warping to %d,%d\n", x, y);
@@ -1044,6 +1046,11 @@ BOOL WAYLAND_ClipCursor(const RECT *clip, BOOL reset)
 
     NtUserGetCursorPos(&cursor_pos);
     hwnd = NtUserGetForegroundWindow();
+
+    /* the cursor pos may have changed between SetCursorPos and ClipCursor calls */
+    pthread_mutex_lock(&pointer->mutex);
+    if (pointer->pending_warp) cursor_pos = pointer->warp;
+    pthread_mutex_unlock(&pointer->mutex);
 
     if (!(data = wayland_win_data_get(hwnd))) return FALSE;
     if ((surface = data->wayland_surface))
